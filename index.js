25bbd778f71905529a095448ed04d39a43c37fdd
vnechit <vnechit310301@gmail.com>
2020-10-07 22:01:28 +0300

    Commit test 2

--- index.js
+++ index.js
@@ -7,6 +7,11 @@
 module.exports = function (hours, minutes, interval) {
     minutes += interval;
     if (minutes > 59){
-
+        hours += Math.floor(minutes/60);
+        minutes %= 60;
     }
+    if (hours > 23) {
+        hours %= 24;
+    }
+    return hours.toString().padStart(2, '0') + ":" + minutes.toString().padStart(2, '0');
 };
