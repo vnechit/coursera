d8d86bb706f627a3f7c59f5ccb4db42c2ea157f1
vnechit <vnechit310301@gmail.com>
2020-10-08 18:43:55 +0300

    new commit

--- index.js
+++ index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function (tweet) {
 
+    var splittedString = tweet.split(' ');
+    var result = [];
+
+    for (var i = 0; i < splittedString.length; i++){
+        if (splittedString[i].startsWith('#')){
+            result.push(splittedString[i].slice(1, splittedString[i].length));
+        }
+    }
+
+    return result;
+
 };
