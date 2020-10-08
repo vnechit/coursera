c6c00aad7f0377936a5d610818964b1d8a41a644
vnechit <vnechit310301@gmail.com>
2020-10-08 19:16:39 +0300

    new commit 3

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
+// Телефонная книга
+var phoneBook = {};
+
 /**
- * @param {String[]} hashtags
- * @returns {String}
+ * @param {String} command
+ * @returns {*} - результат зависит от команды
  */
-module.exports = function (hashtags) {
-
-    var unreapettedHashtags = [];
-
-    if (hashtags.length == 0){
-        return '';
-    }
-
-    for (var i = 0; i < hashtags.length; i++){
-
-        var hashtag = hashtags[i].toLowerCase();
-        if (!unreapettedHashtags.includes(hashtag)){
-            unreapettedHashtags.push(hashtag);
-        }
-
-    }
-
-    return unreapettedHashtags.join(', ');
+module.exports = function (command) {
 
 };
