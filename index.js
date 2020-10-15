7ba95c74bf428f4427d35d93fc463d93c7746e35
vnechit <vnechit310301@gmail.com>
2020-10-15 18:29:23 +0300

    new commit 5

--- index.js
+++ index.js
@@ -1,75 +1,77 @@
-// Телефонная книга
-var phoneBook = {};
-
 /**
- * @param {String} command
- * @returns {*} - результат зависит от команды
+ * @param {String} date
+ * @returns {Object}
  */
-module.exports = function (command) {
+module.exports = function (date) {
+    var dateArr = date.match(/\d{1,4}/g, date);
+    var tmpdate = new Date(Date.UTC(dateArr[0], dateArr[1]-1, dateArr[2], dateArr[3], dateArr[4], 0, 0));
 
-    /**
-     *
-     * @param {String} _name
-     * @param {String} _numbers
-     * @
-     */
-    function Add(_name, _numbers){
-        let numbers = _numbers.split(',');
-        if (phoneBook.hasOwnProperty(_name)){
-            phoneBook[_name] = phoneBook[_name].concat(numbers);
-        } else {
-            phoneBook[_name] = numbers;
-        }
-    }
-
-    /**
-     *
-     * @param {String} _number
-     * @returns {Boolean}
-     */
-    function Remove_Number(_number){
-        let obj = Object.keys(phoneBook);
-        for (let key in obj){
-            let pos = phoneBook[key].indexOf(_number);
-            if (pos !== -1){
-                phoneBook[key].splice(pos, 1);
-                if (phoneBook[key].length === 0){
-                    delete phoneBook[key];
-                }
-                return true;
+    var dateObject = {
+        value: String(dateArr[0])+'-'+String(dateArr[1])+'-'+String(dateArr[2])+' '+String(dateArr[3])+':'+String(dateArr[4]),
+        add: function (num, str) {
+            var tmparr = this.value.match(/\d{1,4}/g, this.value);
+            var tmpd = new Date(Date.UTC(tmparr[0], tmparr[1]-1, tmparr[2], tmparr[3], tmparr[4], 0, 0));
+            if (num < 0) {
+                throw new TypeError('Передано неверное значение: ' + num);
             }
-        }
-        return false;
-    }
 
-    /**
-     *
-     * @returns {Array}
-     */
-    function Show(){
-        let res = [];
-        let obj = Object.keys(phoneBook);
-        for (let key in obj){
-            res.push(key + ': ' + phoneBook[key].join(', '));
-        }
-        res.sort();
-        return res;
-    }
+            if (str === 'hours') {
+                tmpd.setUTCHours(tmpd.getUTCHours() + num);
+            } else if (str === 'years') {
+                tmpd.setUTCFullYear(tmpd.getUTCFullYear() + num);
+            } else if (str === 'months') {
+                tmpd.setUTCMonth(tmpd.getUTCMonth() + num);
+            } else if (str === 'days') {
+                tmpd.setUTCDate(tmpd.getUTCDate() + num);
+            } else if (str === 'minutes') {
+                tmpd.setUTCMinutes(tmpd.getUTCMinutes() + num);
+            } else {
+                throw new TypeError('Передано неверное значение: ' + str);
+            }
+            var tmp = tmpd.toLocaleString("ru",{timeZone : "UTC"});
+            tmparr = tmp.match(/\d{1,4}/g, this.value);
+            for (var i = 0; i < tmparr.length; ++i) {
+                if (Number(tmparr[i]) < 10 && tmparr[i].length < 2) {
+                    tmparr[i] = '0' + tmparr[i];
+                }
+            }
+            this.value = tmparr[0]+'-'+tmparr[1]+'-'+tmparr[2]+' '+tmparr[3]+':'+tmparr[4];
+            return this;
+        },
+        subtract: function (num, str) {
+            var tmparr = this.value.match(/\d{1,4}/g, this.value);
+            var tmpd = new Date(Date.UTC(tmparr[0], tmparr[1]-1, tmparr[2], tmparr[3], tmparr[4], 0, 0));
+            if (num < 0) {
+                throw new TypeError('Передано неверное значение: ' + num);
+            }
 
-    let commandArray = command.split(' ');
+            if (str === 'hours') {
+                tmpd.setUTCHours(tmpd.getUTCHours() - num);
+            } else if (str === 'years') {
+                tmpd.setUTCFullYear(tmpd.getUTCFullYear() - num);
+            } else if (str === 'months') {
+                tmpd.setUTCMonth(tmpd.getUTCMonth() - num);
+            } else if (str === 'days') {
+                tmpd.setUTCDate(tmpd.getUTCDate() - num);
+            } else if (str === 'minutes') {
+                tmpd.setUTCMinutes(tmpd.getUTCMinutes() - num);
+            } else {
+                throw new TypeError('Передано неверное значение: ' + str);
+            }
+            var tmp = tmpd.toLocaleString("ru",{timeZone : "UTC"});
+            tmparr = tmp.match(/\d{1,4}/g, this.value);
+            for (var i = 0; i < tmparr.length; ++i) {
+                if (Number(tmparr[i]) < 10 && tmparr[i].length < 2) {
+                    tmparr[i] = '0' + tmparr[i];
+                }
+            }
+            this.value = tmparr[0]+'-'+tmparr[1]+'-'+tmparr[2]+' '+tmparr[3]+':'+tmparr[4];
+            return this;
+        }
 
-    switch (commandArray[0]) {
-        case 'ADD':{
-            Add(commandArray[1], commandArray[2]);
-            break;
-        }
-        case 'REMOVE_NUMBER':{
-            return Remove_Number(commandArray[1]);
-            break;
-        }
-        case 'SHOW':{
-            return Show();
-            break;
-        }
-    }
+    };
+
+
+    return dateObject;
+
 };
